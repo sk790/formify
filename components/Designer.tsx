09e197020243effde410e9ade4571929720d8d1e
sk790 <saurabhk2890@gmail.com>
2024-07-20 00:06:12 +0530

    added two svg files for designer component

---
+++ components/Designer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Designer() {
+  return <div className="flex w-full h-full">Designer</div>;
+}
+
+export default Designer;
--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -1,28 +1,36 @@
+"use client";
+
 import { Form } from "@prisma/client";
 import React from "react";
 import PreviewDialogBtn from "./PreviewDialogBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import PublishedFormBtn from "./PublishedFormBtn";
+import Designer from "./Designer";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
-    <nav className="flex flex-col w-full">
-      <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex gap-2 items-center">
-          <PreviewDialogBtn />
-          {!form.published && (
-            <>
-              <SaveFormBtn />
-              <PublishedFormBtn />
-            </>
-          )}
+    <main className="flex flex-col w-full">
+      <nav className="flex flex-col w-full">
+        <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex gap-2 items-center">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn />
+                <PublishedFormBtn />
+              </>
+            )}
+          </div>
         </div>
+      </nav>
+      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-hidden h-[200p] bg-accent bg-[url(/rails.svg)] dark:bg-[url(/rails-dark.svg)]">
+        <Designer />
       </div>
-    </nav>
+    </main>
   );
 }
 
