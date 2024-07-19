449a1a5ce443b89454f98faff8f0fa5e9fd0ea0d
sk790 <saurabhk2890@gmail.com>
2024-07-20 03:57:00 +0530

    wraped our formbuilder component in dndcontext and also created a sidebar element toolbar

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import DesignerToolbar from "./DesignerToolbar";
+import { useDroppable } from "@dnd-kit/core";
 
 function Designer() {
-  return <div className="flex w-full h-full">
-    <div className="p-4 w-full">
-        <div className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col"></div>
+  const droppable = useDroppable({
+    id: "designer-droppable",
+    data: {
+      isDesignerDropArea: true,
+    },
+  });
+  return (
+    <div className="flex w-full h-full">
+      <div className="p-4 w-full">
+        <div className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1">
+          <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+            Drop here
+          </p>
+        </div>
+      </div>
+      <DesignerToolbar />
     </div>
-  </div>;
+  );
 }
 
 export default Designer;
--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -6,31 +6,34 @@ import PreviewDialogBtn from "./PreviewDialogBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import PublishedFormBtn from "./PublishedFormBtn";
 import Designer from "./Designer";
+import { DndContext } from "@dnd-kit/core";
 
 function FormBuilder({ form }: { form: Form }) {
   return (
-    <main className="flex flex-col w-full">
-      <nav className="flex flex-col w-full">
-        <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
-          <h2 className="truncate font-medium">
-            <span className="text-muted-foreground mr-2">Form:</span>
-            {form.name}
-          </h2>
-          <div className="flex gap-2 items-center">
-            <PreviewDialogBtn />
-            {!form.published && (
-              <>
-                <SaveFormBtn />
-                <PublishedFormBtn />
-              </>
-            )}
+    <DndContext>
+      <main className="flex flex-col w-full">
+        <nav className="flex flex-col w-full">
+          <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
+            <h2 className="truncate font-medium">
+              <span className="text-muted-foreground mr-2">Form:</span>
+              {form.name}
+            </h2>
+            <div className="flex gap-2 items-center">
+              <PreviewDialogBtn />
+              {!form.published && (
+                <>
+                  <SaveFormBtn />
+                  <PublishedFormBtn />
+                </>
+              )}
+            </div>
           </div>
+        </nav>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-hidden h-[200p] bg-accent bg-[url(/rails.svg)] dark:bg-[url(/rails-dark.svg)]">
+          <Designer />
         </div>
-      </nav>
-      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-hidden h-[200p] bg-accent bg-[url(/rails.svg)] dark:bg-[url(/rails-dark.svg)]">
-        <Designer />
-      </div>
-    </main>
+      </main>
+    </DndContext>
   );
 }
 
