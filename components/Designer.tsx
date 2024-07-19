02e44fa4833fd3a6f3b2d12e492014afc00f9eed
sk790 <saurabhk2890@gmail.com>
2024-07-20 00:12:07 +0530

    installed dnd kit for draging and droping

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Designer() {
-  return <div className="flex w-full h-full">Designer</div>;
+  return <div className="flex w-full h-full">
+    <div className="p-4 w-full">
+        <div className="bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col"></div>
+    </div>
+  </div>;
 }
 
 export default Designer;
