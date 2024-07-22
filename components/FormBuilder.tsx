4d323af9d227be982e4ce592789fd547c36412e8
sk790 <saurabhk2890@gmail.com>
2024-07-22 08:29:46 +0530

    added react Confetti in formbuilder after form publish

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -15,6 +15,12 @@ import {
 } from "@dnd-kit/core";
 import DragOverlayWrapper from "./DragOverlayWrapper";
 import useDesigner from "./hooks/useDesigner";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
+import Link from "next/link";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Confetti from "react-confetti";
 
 function FormBuilder({ form }: { form: Form }) {
   const { setElements } = useDesigner();
@@ -36,6 +42,56 @@ function FormBuilder({ form }: { form: Form }) {
     const elements = JSON.parse(form.content);
     setElements(elements);
   }, [form, setElements]);
+
+  const shareLink = `${window.location.origin}/submit/${form.shareURL}`;
+
+  if (form.published) {
+    return (
+      <>
+        <Confetti />
+        <div className="flex flex-col items-center justify-center h-full w-full">
+          <div className="max-w-md">
+            <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10">
+              Form Published
+            </h1>
+            <h2 className="text-2xl">Share this form</h2>
+            <h3 className="text-xl text-muted-foreground border-b pb-10">
+              Anyone with the link can view and submit the form
+            </h3>
+            <div className="my-4 flex flex-col gap-2 items-center w-full border-b pb-4">
+              <Input className="w-full" readOnly value={shareLink} />
+              <Button
+                className="w-full mt-2"
+                onClick={() => {
+                  navigator.clipboard.writeText(shareLink);
+                  toast({
+                    title: "Copied",
+                    description: "Link Copied to clipboard",
+                  });
+                }}
+              >
+                Link Copy
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button variant={"link"} asChild>
+                <Link href={"/"} className="gap-2">
+                  <BsArrowLeft />
+                  Go back home
+                </Link>
+              </Button>
+              <Button variant={"link"} asChild>
+                <Link href={`/form/${form.id}`} className="gap-2">
+                  Form details
+                  <BsArrowRight />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <DndContext sensors={sensors}>
