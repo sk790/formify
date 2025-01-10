c16703774e2b83cd5d8e39a0856b315c1f6d24da
sk790 <saurabhk2890@gmail.com>
2025-01-10 22:29:38 +0530

    correct spelling of head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import DesignerContextProvider from "@/components/context/DesignerContext";
 import NextToploader from "nextjs-toploader";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Formify",
@@ -21,32 +21,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <ClerkProvider>
-        <html lang="en">
-          <Head>
-            <script
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3773468401628725"
-              crossOrigin="anonymous"
-            ></script>
-          </Head>
-          <body className={inter.className}>
-            <NextToploader />
-            <DesignerContextProvider>
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-              >
-                {children}
-                <Toaster />
-              </ThemeProvider>
-            </DesignerContextProvider>
-          </body>
-        </html>
-      </ClerkProvider>
-    </>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3773468401628725"
+            crossOrigin="anonymous"
+            strategy="afterInteractive" // Ensures script loads after page is interactive
+          />
+        </head>
+        <body className={inter.className}>
+          <NextToploader />
+          <DesignerContextProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </DesignerContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
